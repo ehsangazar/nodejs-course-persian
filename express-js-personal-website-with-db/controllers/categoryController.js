8ce1ac78267a65c077e93201d77f1982038b59b8
Ehsan Gazar <me@ehsangazar.com>
2021-08-23 20:32:30 +1000

    fix tabs and pages and categories

--- express-js-personal-website-with-db/controllers/categoryController.js
+++ express-js-personal-website-with-db/controllers/categoryController.js
@@ -1,14 +1,18 @@
 const Category = require('../models/Category')
 const Post = require('../models/Post')
 
-const homepageController = async (req, res) => {
+const categoryController = async (req, res) => {
   const categories = await Category.findAll()
-  const posts = await Post.findAll()
+  const posts = await Post.findAll({
+    where: {
+      category_id: Number(req.params.id) + 1,
+    },
+  })
   res.render('index', {
     categories: categories.map((category) => category.name),
     posts: posts,
-    activeId: null,
+    activeId: Number(req.params.id),
   })
 }
 
-module.exports = homepageController
+module.exports = categoryController
--- express-js-personal-website-with-db/routes/index.js
+++ express-js-personal-website-with-db/routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const homepageController = require('../controllers/homepageController')
+const postController = require('../controllers/postController')
 const aboutController = require('../controllers/aboutController')
 const contactController = require('../controllers/contactController')
+const categoryController = require('../controllers/categoryController')
 
 router.get('/', homepageController)
+router.get('/post/:id', postController)
+router.get('/category/:id', categoryController)
 router.get('/about', aboutController)
 router.get('/contact', contactController)
 
